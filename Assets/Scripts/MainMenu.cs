89b802e651a4b7f4b1d453a95de2357aa9998328
Oleg Korneev <waterpoleg@gmail.com>
2020-08-27 18:48:23 +0300

    added 2 scenes: mainmenu & level, added mainmenu script

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void OnPlayButtonClick()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnQuitButtonQlick()
+    {
+        Application.Quit();
+    }
+}
