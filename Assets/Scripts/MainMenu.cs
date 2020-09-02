391d5eec0ce1c795e322e2bbc5d37e4b63869228
Oleg Korneev <waterpoleg@gmail.com>
2020-09-02 18:23:07 +0300

    added ctedits window & main menu action

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,11 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject _creditsWindow;
+
     public void OnPlayButtonClick()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void OnCreditsButtonClick()
+    {
+        _creditsWindow.SetActive(true);
+    }
+
+    public void OnCreditsWiondowClick()
+    {
+        _creditsWindow.SetActive(false);
     }
 
     public void OnQuitButtonQlick()
